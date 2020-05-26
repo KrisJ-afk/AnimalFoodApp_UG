ee391d8fec229c7a9f9f487fd8b0f04c8cfa975d
KrisJ-afk <goodreadsBaconPancakes@gmail.com>
2020-05-26 17:29:08 +0200

    added simple menu

--- AnimalFood/AnimalFood/App.xaml.cs
+++ AnimalFood/AnimalFood/App.xaml.cs
@@ -10,7 +10,7 @@ namespace AnimalFood
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MainContentPage();
         }
 
         protected override void OnStart()
---
+++ AnimalFood/AnimalFood/Views/PageAbout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimalFood.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAbout : ContentPage
+    {
+        public PageAbout()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
